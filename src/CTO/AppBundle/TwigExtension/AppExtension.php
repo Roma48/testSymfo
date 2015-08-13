8a1854e31e5c5821e37fe64f8971122bd15133ed
Yuriy Tarnavskiy <etheriq2@gmail.com>
2015-08-14 02:46:11 +0300

    show in broadcast item (to)

--- src/CTO/AppBundle/TwigExtension/AppExtension.php
+++ src/CTO/AppBundle/TwigExtension/AppExtension.php
@@ -2,12 +2,24 @@
 
 namespace CTO\AppBundle\TwigExtension;
 
+use CTO\AppBundle\Entity\CtoClient;
+use Doctrine\ORM\EntityManager;
+
 class AppExtension extends \Twig_Extension
 {
+    /** @var EntityManager $em */
+    protected $em;
+
+    public function __construct(EntityManager $manager)
+    {
+        $this->em = $manager;
+    }
+
     public function getFilters()
     {
         return array(
             new \Twig_SimpleFilter('monthTranslate', array($this, 'monthTranslateFilter')),
+            new \Twig_SimpleFilter('userNames', array($this, 'transformIdToUsernames')),
         );
     }
 
@@ -29,6 +41,25 @@ class AppExtension extends \Twig_Extension
         $month['December'] = "Грудень";
 
         return $month[$tmpDate[0]]. ', ' . $tmpDate[1];
+    }
+
+    public function transformIdToUsernames($data)
+    {
+        $iDs = explode(',', $data);
+
+        $result = '';
+        if (in_array('-1', $iDs)) {
+            $result = "Розіслати всім";
+        } else {
+            $users = $this->em->getRepository('CTOAppBundle:CtoClient')->findBy(['id' => $iDs]);
+
+            /** @var CtoClient $user */
+            foreach ($users as $user) {
+                $result .= "<div class='text-left'>" . $user->getFullName() . "</div>";
+            }
+        }
+
+        return $result;
     }
 
     /**
